a31080c590779d631a33f205b5843ee356515ab5
ITrunner1 <MatveyT1337@yandex.ru>
2024-05-17 14:06:24 +1000

    Change subscription entity

--- src/user/subscription.entity.ts
+++ src/user/subscription.entity.ts
@@ -4,13 +4,12 @@ import { Entity, JoinColumn, ManyToOne, OneToMany } from "typeorm";
 import { UserEntity } from "./user.entity";
 
 @Entity('Subscription')
-export class SubscriptionEntity extends Base {
-    
+export class SubscriptionEntity extends Base {    
     @ManyToOne(() => UserEntity, user => user.subscription)
     @JoinColumn({name: 'from_user_id'})
     fromUser: UserEntity
 
-    @OneToMany(() => SongEntity, song => song.user)
+    @ManyToOne(() => UserEntity, user => user.subscription)
     @JoinColumn({name: 'to_artist_id'})
-    toArtist: UserEntity[]
+    toChannel: UserEntity
 }
