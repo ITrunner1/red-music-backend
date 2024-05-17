dfc800baa8b6d987e8122d657bc02ac9c17a81c7
ITrunner1 <MatveyT1337@yandex.ru>
2024-05-17 19:23:12 +1000

    Adde comment logic

--- src/comment/comment.controller.ts
+++ src/comment/comment.controller.ts
@@ -1,7 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, HttpCode, Post, UsePipes, ValidationPipe } from '@nestjs/common';
 import { CommentService } from './comment.service';
+import { CurrentUser } from 'src/decorators/user.decorator';
+import { CommentDto } from './comment.dto';
+import { Auth } from 'src/decorators/auth.decorator';
 
 @Controller('comment')
 export class CommentController {
   constructor(private readonly commentService: CommentService) {}
+
+  @UsePipes(new ValidationPipe())
+  @HttpCode(200)
+  @Post()
+  @Auth()
+  async createComment(@CurrentUser('id') id: string, @Body() dto: CommentDto) {
+    return this.commentService.create(Number(id), dto)
+  }
 }
