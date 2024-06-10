a132203b4e1643b0f6141fa2c0cbf9a84135d769
ITrunner1 <MatveyT1337@yandex.ru>
2024-06-11 01:24:46 +1000

    Add server ip address

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors()
+  app.enableCors({
+    origin: [
+      'http://localhost:3000', 'https://89.248.206.108:3000'
+    ],
+    credentials: true,
+    exposedHeaders: 'set-cookie'
+  })
   app.setGlobalPrefix('api');
   await app.listen(4200);
 }
